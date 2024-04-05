27284942ad8a1904b3853b42d0c5c813f6fd1510
filip <grujicf998@gmail.com>
2024-04-05 13:26:05 +0200

    Update example scenario to work properly with the London fork

--- examples/EOA.js
+++ examples/EOA.js
@@ -2,13 +2,13 @@ import eth from 'k6/x/ethereum';
 import exec from 'k6/execution';
 
 export const options = {
-    vus: 5,
+    vus: 6,
     scenarios: {
         constant_request_rate: {
             executor: 'constant-arrival-rate',
             duration: '1m',
-            preAllocatedVUs: 5,
-            rate: 300,
+            preAllocatedVUs: 6,
+            rate: 500,
             timeUnit: '1s'
         }
     }
@@ -19,16 +19,21 @@ export function setup() {
 }
 
 var client
+var gasPrice
 
 export default function (data) {
     if (client == null) {
         client = new eth.Client(data[exec.vu.idInTest - 1].PrivateKey)
     }
 
+    if (exec.vu.iterationInInstance == 0 || exec.vu.iterationInInstance % 500 == 0) {
+        gasPrice = client.gasPrice()
+    }
+
     const tx = {
         to: "0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF",
         value: 100,
-        gas_price: client.gasPrice() * 1.2,
+        gas_price: gasPrice * 1.2,
     };
 
     client.sendRawTransaction(tx)
